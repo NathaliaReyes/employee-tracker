f7a6cc96f55ecd16db70c6cacb4e9b638ea2cf55
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-03-15 18:29:58 -0600

    adding new file banner.js and functionality

--- server.js
+++ server.js
@@ -1,53 +1,22 @@
-const express = require('express');
+// const express = require('express');
 const CLI = require('./lib/cli.js');
 
 const cli = new CLI();
 
-const { Pool } = require('pg');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-
-// Connect to the database:
-const pool = new Pool(
-    {
-      user: 'postgres',
-      password: 'lacasadelarbolCODING23',
-      host: 'localhost',
-      database: 'tracker_db'
-    },
-    console.log(`Connected to the movie_db database.`)
-)
-
-pool.connect();
-
-// app.get('/api/movies', (req, res) => {
-//     pool.query('SELECT id, nombre AS title FROM movies', (err, { rows }) => {
-//         if(err) {
-//             throw err;
-//             return;
-//         } 
-//         res.json({
-//             message: 'success!!',
-//             data: rows
-//         });
-//     });
-// });
-
-
-app.use((req, res) => {
-    res.status(404).end();
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3001;
 
 cli.run();
 
-module.exports = { pool };
+
+
+
+// app.use((req, res) => {
+//     res.status(404).end();
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
+
+

8a0697f40b90df19c65e4360ad6814acea35b675
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-03-15 19:44:46 -0600

    editing function to showBanner from fs.readFile() which is async to fs.readFileSync()

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // const express = require('express');
 const CLI = require('./lib/cli.js');
 
+const { readBanner } = require('./lib/banner.js');
+
 const cli = new CLI();
 
 // const PORT = process.env.PORT || 3001;
-
+readBanner();
 cli.run();
 
 

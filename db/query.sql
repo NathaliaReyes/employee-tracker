b495aa12ed9ce4dfd3c620ba091fc40bc3ced1f4
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-03-20 13:57:19 -0600

    Updating method getEmployeesByManager()

--- db/query.sql
+++
@@ -1,6 +0,0 @@
-SELECT employees.id, employees.first_name, employees.last_name, roles.title, 
-departments.nombre AS department, roles.salary, CONCAT(employees.first_name, ' ', employees.last_name)  AS manager 
-FROM employees 
-JOIN roles ON employees.role_id = roles.id 
-JOIN departments ON roles.department = departments.id 
-ORDER BY employees.id
